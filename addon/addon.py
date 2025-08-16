acf914c2fecf2b778eec76f1fadd4a26fe0d85b8
kalemtzis <kalemtzis@csd.auth.gr>
2025-08-17 00:34:50 +0300

    02: fix directories

--- addon/addon.py
+++ addon/addon.py
@@ -1,19 +1,7 @@
-#import xbmcplugin
-#import xbmcgui
-#import xbmcaddon
-#import sys
 import json
 import os
 import requests
 
-#addon_handle = int(sys.argv[1])
-#addon = xbmcaddon.Addon()
-
-#ADDON_PATH = xbmcaddon.Addon().getAddonInfo('path')
-#TOKEN_FILE = os.path.join(ADDON_PATH, 'resources', 'token.json')
-
-#CLIENT_ID = 'a1bba716bfeff8697b1c1a5e813329d4c2cc7f9e683800a2c624e8e62930bc8f'
-#CLIENT_SECRET = 'b0087bece91d8f77eedcfa23b1b6cb760c230644b4e7ff42a5c69433884159df'
 CLIENT_ID = 'e0bbeac986d4711b5f29e40dd3b58a25f673d5f8b15f9f6cb7f2ef4fe8896d2a'
 CLIENT_SECRET = '45768e6434ad9d5935dd8e8dbc7188a9f9d4ab5ab407fa843783668b96c5cdee'
 REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
@@ -28,11 +16,6 @@ def autheticate():
   auth_url = f"https://trakt.tv/oauth/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
   
   print(f"Go to this URL in your browser, login and get a PIN:\n{auth_url}")
-  
-  #xbmcgui.Dialog().ok("Trakt Authentication", f"Visit this URL:\n{auth_url}\n\nEnter the PIN code below in the next step.")
-  
-  #keyboard = xbmcgui.Dialog().input("Enter Trakt PIN")
-  #pin = keyboard.strip()
   
   pin = input(f"Enter your PIN from Trakt: ").strip()
   
@@ -69,7 +52,7 @@ def get_liked_lists(tokens):
     "trakt-api-key": CLIENT_ID
   }
   
-  res = requests.get(f"{TRAKT_API_ENDPOINT}/users/me/lists", headers=headers)
+  res = requests.get(f"{TRAKT_API_ENDPOINT}/users/likes/lists", headers=headers)
   res.raise_for_status()
   
   
@@ -93,14 +76,26 @@ def get_list_movies(tokens, username, list_id):
 if __name__ == '__main__':
   tokens = autheticate()
   username = get_username(tokens)
+  print("Username: ", username)
   liked_lists = get_liked_lists(tokens)
   
-  print(json.dumps(liked_lists, indent=2))
-  
-  all_movies = []
   for lst in liked_lists:
-    list_id = lst["ids"]["slug"]
-    movies = get_list_movies(tokens, username, list_id)
-    all_movies.extend(movies)
+    print(lst['list']['name'])  
+  
+  #all_movies = []
+  for lst in liked_lists:
+    list_id = lst['list']["ids"]["slug"]
+    creatorUsername = lst['list']['user']['username']
+    list_name = lst['list']['name']
+    
+    movies = get_list_movies(tokens, creatorUsername, list_id)
+    
+    os.makedirs(f"{list_name}", exist_ok=True)
+    list_dir = os.path.join(os.getcwd(), list_name)
+    
+    with open(os.path.join(list_dir, f"{list_name}.json"), 'w', encoding='utf-8') as f:
+      json.dump(movies, f, indent=2)
+    #all_movies.extend(movies)
 
-  print(json.dumps(all_movies, indent=2))
+  #with open('list.json', 'w') as f:
+    # json.dump(all_movies, f, indent=2)
